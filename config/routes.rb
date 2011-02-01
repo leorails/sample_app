e463c5d5cf79bcf9cadae8dd9c6d7e98d12db02b
sage <leorails@gmail.com>
2011-02-01 14:40:24 -0800

    Chapter 5 Added CSS and Named Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  #get "pages/home"
+  #get "pages/contact"
+  #get "pages/about"
+  #get "pages/help"
 
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
-
+  #match '/', :to => 'pages#home'  # not necessary
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,7 +58,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
