96ee549b28d24f5e13951dc7337aed786f2eeb7c
sage <leorails@gmail.com>
2011-02-02 17:44:15 -0800

    End of day 2011-02-02

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require 'digest'
 class User < ActiveRecord::Base
-
-  attr_accessible( :name, :email)
+  attr_accessor :password
+  
+  attr_accessible( :name, :email, :password, :password_confirmation)
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -20,5 +21,42 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
 
+  # ActiveRecord Callback right before saving the record
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
