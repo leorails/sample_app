11e6c218066756d2db23fe003871dbae203cbba4
sage <leorails@gmail.com>
2011-02-11 03:04:02 +0800

    Change database to mysql

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ gem 'rails', '3.0.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+#gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem 'mysql2'
+
 gem 'gravatar_image_tag', '1.0.0.pre2'
 gem 'will_paginate', '3.0.pre2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(:version => 20110208051709) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
-    t.boolean  "admin"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
